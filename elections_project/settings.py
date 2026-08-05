82f5963c5e4a5a6b32b5c57298fdbb78614ec171
Joshua Kapilya <joshuakapilya15@gmail.com>
2026-08-05 01:26:21 -0700

    latest commit

--- elections_project/settings.py
+++ elections_project/settings.py
@@ -146,14 +146,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
